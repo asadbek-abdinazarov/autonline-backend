f361123a3a0e95ac2e6482db601680c0f5d5c9ed
Asadbek <asadbekabdunazarov602@gmail.com>
2025-12-09 17:41:24 +0500

    S3 Bucked adding for Question images

---
+++ src/main/java/uz/javachi/autonline/controller/StorageController.java
@@ -0,0 +1,48 @@
+package uz.javachi.autonline.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import uz.javachi.autonline.service.StorageService;
+
+import java.net.URLConnection;
+
+@RestController
+@RequestMapping("/api/v1/storage")
+@RequiredArgsConstructor
+public class StorageController {
+
+    private final StorageService storageService;
+
+    @PostMapping("/upload")
+    @PreAuthorize("hasAnyRole('ADMIN', 'TEACHER')")
+    public ResponseEntity<String> upload(@RequestParam MultipartFile file) {
+        String key = storageService.uploadFile(file, "images");
+        return ResponseEntity.ok(key);
+    }
+
+    @GetMapping("/file")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'TEACHER')")
+    public ResponseEntity<byte[]> getFile(@RequestParam String key) {
+        byte[] fileData = storageService.downloadFile(key);
+
+        String contentType = URLConnection.guessContentTypeFromName(key);
+        if (contentType == null) contentType = "application/octet-stream";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(fileData);
+    }
+
+
+
+    @DeleteMapping("/delete")
+    @PreAuthorize("hasAnyRole('ADMIN', 'TEACHER')")
+    public ResponseEntity<String> delete(@RequestParam String key) {
+        storageService.deleteFile(key);
+        return ResponseEntity.ok("Deleted");
+    }
+}
---
+++ src/main/java/uz/javachi/autonline/service/StorageService.java
@@ -0,0 +1,69 @@
+package uz.javachi.autonline.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class StorageService {
+
+    private final S3Client s3Client;
+
+    @Value("${do.space.bucket}")
+    private String bucket;
+
+    public byte[] downloadFile(String key) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        return s3Client.getObjectAsBytes(getObjectRequest).asByteArray();
+    }
+
+
+    public String uploadFile(MultipartFile file, String folder) {
+
+        StringBuilder key = new StringBuilder();
+        key.append(key)
+                .append(folder)
+                .append("/")
+                .append(UUID.randomUUID())
+                .append("_")
+                .append(file.getOriginalFilename());
+
+        String resultKey = key.toString();
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(resultKey)
+                .acl("private")
+                .contentType(file.getContentType())
+                .build();
+
+        try {
+            s3Client.putObject(request, RequestBody.fromBytes(file.getBytes()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return resultKey;
+    }
+
+    public void deleteFile(String key) {
+        s3Client.deleteObject(DeleteObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build()
+        );
+    }
+}
